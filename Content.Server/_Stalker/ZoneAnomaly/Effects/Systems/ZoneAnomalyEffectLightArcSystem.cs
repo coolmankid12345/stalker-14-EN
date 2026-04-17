c325354cf413aa3bb23cec44260d0b40a464d284
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2026-04-17 11:19:21 -0600

    Okay yes

--- Content.Server/_Stalker/ZoneAnomaly/Effects/Systems/ZoneAnomalyEffectLightArcSystem.cs
+++ Content.Server/_Stalker/ZoneAnomaly/Effects/Systems/ZoneAnomalyEffectLightArcSystem.cs
@@ -1,12 +1,12 @@
-using System.Linq; // Stalker-EN
 using Content.Server.Lightning;
+using Content.Server.Power.Components;
+using Content.Server.Power.EntitySystems;
 using Content.Shared._Stalker.ZoneAnomaly.Components;
 using Content.Shared._Stalker.ZoneAnomaly.Effects.Components;
 using Content.Shared.Power.Components;
 using Content.Shared.Power.EntitySystems;
 using Content.Shared.Whitelist;
-using NetCord;
-using Robust.Shared.Random;
+using Robust.Shared.Map.Components;
 
 namespace Content.Server._Stalker.ZoneAnomaly.Effects.Systems;
 
@@ -18,41 +18,37 @@ public sealed class ZoneAnomalyEffectLightArcSystem : EntitySystem
     [Dependency] private readonly EntityLookupSystem _lookup = default!;
     [Dependency] private readonly LightningSystem _lightning = default!;
     [Dependency] private readonly EntityWhitelistSystem _whitelistSystem = default!;
-    [Dependency] private readonly IRobustRandom _random = default!; // Stalker-EN changes
 
     public override void Initialize()
     {
         SubscribeLocalEvent<ZoneAnomalyEffectLightArcComponent, ZoneAnomalyActivateEvent>(OnActivate);
     }
 
-    // Stalker-EN changes - We rewrote everything below here, dont take upstream changes
     private void OnActivate(Entity<ZoneAnomalyEffectLightArcComponent> effect, ref ZoneAnomalyActivateEvent args)
     {
-        var entities = _lookup.GetEntitiesInRange(Transform(effect).Coordinates,
-            effect.Comp.Distance,
-            flags: LookupFlags.Uncontained);
-
         var i = 0;
+        var entities = _lookup.GetEntitiesInRange(Transform(effect).Coordinates, effect.Comp.Distance, LookupFlags.Uncontained);
         foreach (var entity in entities)
         {
-            if (i >= MaxIterations)
+            if (i > MaxIterations)
                 break;
 
-            if (!_whitelistSystem.IsWhitelistPass(effect.Comp.Whitelist, entity))
+            // We don't need to shoot all the entities
+            if(!_whitelistSystem.IsWhitelistPass(effect.Comp.Whitelist, entity))
                 continue;
 
             TryRecharge(effect, entity);
             _lightning.ShootLightning(effect, entity, effect.Comp.Lighting);
+
             i++;
         }
     }
 
     private void TryRecharge(Entity<ZoneAnomalyEffectLightArcComponent> effect, Entity<PredictedBatteryComponent?> target)
     {
-        if(!Resolve(target, ref target.Comp, false))
+        if (!Resolve(target, ref target.Comp))
             return;
 
-        _battery.SetCharge(target, target.Comp.LastCharge + target.Comp.MaxCharge * effect.Comp.ChargePercent);
+        _battery.SetCharge((target, target.Comp), target.Comp.LastCharge + target.Comp.MaxCharge * effect.Comp.ChargePercent);
     }
-    // stalker-en-end
 }
